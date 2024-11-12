85c76ed9b6b7309fdd5724a7f530e4460385d942
Erik.Jawad <erik.jawad@elev.ga.ntig.se>
2024-11-12 11:12:52 +0100

    mjhgkuyf

--- obj/Debug/net8.0/NäsanÖverE.AssemblyInfo.cs
+++ obj/Debug/net8.0/NäsanÖverE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NäsanÖverE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7210763f0a30c454e346f313de4ebd931409ecc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NäsanÖverE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NäsanÖverE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
